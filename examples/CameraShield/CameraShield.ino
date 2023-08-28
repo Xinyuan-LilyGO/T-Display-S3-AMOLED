f565930f333f0d81bc019c496d3052e18cf72314
lewishe <lewishe@outlook.com>
2023-08-28 10:28:26 +0800

    Fixed typo

--- examples/CameraShield/CameraShield.ino
+++ examples/CameraShield/CameraShield.ino
@@ -1,5 +1,5 @@
 /**
- * @file      CameraShiled.ino
+ * @file      CameraShield.ino
  * @author    Lewis He (lewishe@outlook.com)
  * @license   MIT
  * @copyright Copyright (c) 2023  Shenzhen Xin Yuan Electronic Technology Co., Ltd
